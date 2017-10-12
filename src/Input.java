a1b817fd50f87065c059226287ef4228e3729b21
Addono <a.d.knapen@protonmail.com>
2017-10-12 12:02:33 +0200

    new: added support for reading ZmodP objects from the scanner

--- src/Input.java
+++ src/Input.java
@@ -1,11 +1,14 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Input {
 
     private Scanner scanner;
-    private String[] supportedOperations = {"abc", "def"};
+    private String[] supportedOperations = {"abc", "def", "mul"};
+    final private String inputPrefix = " < ";
+    final private String outputPrefix = " > ";
 
     public Input(Scanner scanner) {
         this.scanner = scanner;
@@ -16,11 +19,12 @@ public class Input {
             throw new IllegalStateException("Input_Scanner.read: scanner should be not null");
         }
 
-        System.out.println("First reading the desired operation to be executed");
-        System.out.println("Supported operations are:");
+        System.out.println(outputPrefix + "First reading the desired operation to be executed");
+        System.out.println(outputPrefix + "Supported operations are:");
         for (String s : supportedOperations) {
             System.out.println("\t- '" + s + "'");
         }
+        System.out.println(inputPrefix + "Please enter the desired operation.");
 
         StringBuilder regex = new StringBuilder();
         boolean first = true;
@@ -42,25 +46,71 @@ public class Input {
             try {
                 operation = scanner.next(compile);
             } catch (InputMismatchException e) {
-                System.out.println("Operation is not supported, please try again.");
+                System.out.println(inputPrefix + "Operation is not supported, please try again.");
                 scanner.nextLine();
             }
         }
 
-        System.out.println("Operation: [" + operation + "]");
+        System.out.println(outputPrefix + "Operation selected: [" + operation + "]");
 
+        executeOperation(operation);
+    }
+
+    private void executeOperation(String operation) {
         switch (operation) {
             case "abc":
+
                 System.out.println("Do abc");
                 break;
             case "def":
                 System.out.println("Do def");
                 break;
+            case "mul":
+                System.out.println(outputPrefix + "Expects two ZmodP objects a and b as input with equal p.");
+                System.out.println(outputPrefix + "Returns a ZmodP value object with value (a * b) mod p");
+
+                ZmodP a = readZmodP("a");
+                ZmodP b = readZmodP("b");
+
+                try {
+                    ZmodP result = a.multiply(b);
+                    System.out.println(outputPrefix + result.toString());
+                } catch (IllegalArgumentException e) {
+                    System.out.println(outputPrefix + "FAILED: The p values of both ZmodP objects should be equal.");
+                }
+
+                break;
         }
     }
 
-    private void readZmodP() {
-        // Implement this
+    private ZmodP readZmodP(String name) {
+        System.out.println(inputPrefix + "[" + name + "] Please enter a ZmodP value in the format 'Z mod P' with Z an integer and P a prime.");
+
+        Pattern pattern = Pattern.compile("[ 0]*([1-9]\\d*) *[mod]{1,3} *[ 0]*([1-9]\\d*)");
+
+        Matcher matches = null;
+        do {
+            try {
+                String nextLine = scanner.nextLine();
+
+                matches = pattern.matcher(nextLine);
+
+                // Skip empty lines.
+                if (nextLine.equals("")) {
+                    continue;
+                } else if (! matches.matches()) { // Check if the input matches the expected input.
+                    System.out.println(inputPrefix + "Illegal input given, please try again and mind the format.");
+                    continue;
+                }
+
+                int value = Integer.valueOf(matches.group(1));
+                int p = Integer.valueOf(matches.group(2));
+
+                return new ZmodP(value, p);
+            } catch (IllegalArgumentException e) {
+                System.out.println("P should be prime, please try again.");
+            }
+        } while (true);
     }
 
     private void readPolynomial() {
