4a38af44472472fa9a509459c87bb7d075ab2270
gwvdh <gwvdh97@gmail.com>
2017-10-17 15:54:58 +0200

    Actual tests

---
+++ src/Values/PolynomialTest.java
@@ -0,0 +1,46 @@
+package Values;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolynomialTest {
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sub() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void scarlarMultiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void equals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void longDivision() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void euclid() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void extendedEuclid() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mod() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void equalMod() {
+    }
+
+}
